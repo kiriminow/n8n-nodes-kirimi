be5e63a5f17311ac1f78788bc1e3e876751b7e72
yolkmonday <aripadrian01@gmail.com>
2025-08-12 18:45:51 +0700

    fix: update gulpfile to copy node icons to dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const { src, dest } = require('gulp');
+const { parallel } = require('gulp');
 
 function copyIcons() {
 	return src('icons/**/*').pipe(dest('dist/icons'));
 }
 
+function copyNodeIcons() {
+	return src('nodes/**/*.svg').pipe(dest('dist/nodes'));
+}
+
 exports.copyIcons = copyIcons;
-exports['build:icons'] = copyIcons;
+exports.copyNodeIcons = copyNodeIcons;
+exports['build:icons'] = parallel(copyIcons, copyNodeIcons);
