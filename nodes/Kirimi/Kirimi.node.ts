a0186c8ec712521ca67b795d26390ab75c4538f2
yolkmonday <aripadrian01@gmail.com>
2025-08-12 18:24:29 +0700

    feat: change icon to use external URL from kirimi.id

--- nodes/Kirimi/Kirimi.node.ts
+++ nodes/Kirimi/Kirimi.node.ts
@@ -16,7 +16,7 @@ export class Kirimi implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Kirimi',
 		name: 'kirimi',
-		icon: 'file:kirimi.svg',
+		iconUrl: 'https://kirimi.id/kirimi.svg',
 		group: ['communication'],
 		version: 1,
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
